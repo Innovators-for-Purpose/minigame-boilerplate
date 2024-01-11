e7eff5fa45d60c3baa7b48ee6fc3cfb32fd29817
Henrylesatele <henry.lesatele@gmail.com>
2024-01-11 18:31:27 -0500

    Refining text bubble texture with texture rect.

--- Scenes/Timer.gd
+++ Scenes/Timer.gd
@@ -4,7 +4,9 @@ var inactive = false
 var count = 1
 
 func _on_Timer_timeout():
-	var inactive = true
+	
+	if Input.is_action_just_pressed("game_usage"):
+		var inactive = true
 	count = count+1
 	print(count)
 	if count == 8:
